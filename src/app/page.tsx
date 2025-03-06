86138c38f8825356f7df0d1011b621f331737029
Jose Rosas <xote2000@gmail.com>
2025-03-06 12:22:31 -0600

    uwu listo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
    return (
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
          <Button />
-         <h1>CKA Xote estuvo aca</h1>
+         <h1>CKA Xote estuvo aca uwu</h1>
       </div>
    );
 }
